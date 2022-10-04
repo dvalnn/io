ca0ad905dbd06f6bc6782b41ddca033ec31463d9
dvalinn <tiago.andre.amorim@gmail.com>
2022-10-04 16:11:44 +0100

    moved prob code into a dedicated folder, finished all parts

--- tp3/prob1/metropolis.py
+++ tp3/prob1/metropolis.py
@@ -25,9 +25,14 @@ model = pulp.LpProblem("Escolha_de_locais", pulp.LpMinimize)
 # objective func
 model += sum(l[i] for i in loc)
 
-# restrain
+# restrains
 for zone in zones:
-    model += sum(C[zone][i - 1] * l[i] for i in loc) >= 1
+    model += sum(C[zone][i - 1] * l[i] for i in loc) >= 1, f"Zone_{zone+1} must be covered"
+
+model += l[1] + l[3] + l[4] >= 2, "extra 1"
+model += l[5] + l[6] + l[7] == 2, "extra 2"
+model += l[3] + l[4] <= 1, "extra 3"
+model += l[7] - l[2] >= 0, "extra 4"
 
 status = model.solve()
 
